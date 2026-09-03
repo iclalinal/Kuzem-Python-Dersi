5d0605fbb43a62830fc288ef50c8667563258ec7
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-09-03 20:57:37 +0300

    csv örneği kullanımı

--- ana_program.py
+++ ana_program.py
@@ -1,11 +1,19 @@
-try:
-    with open('gunluk.txt', 'r') as dosya:
-        print("Önceki kayitlar:")
-        for satir in dosya:
-            print(satir.strip())
-except FileNotFoundError:
-    print("ilk kaydınız")
+import csv
+ogrenciler = [
+    ["Ali", 85],
+    ["Ayşe", 90],
+    ["Mehmet", 78],
+    ["Fatma", 92],
+]
 
-yeni_giris = input("Yeni kaydınızı giriniz: ")
-with open('gunluk.txt', 'a') as dosya:
-    dosya.write(yeni_giris + '\n')
+with open("notlar.csv","w", newline="") as dosya:
+    yazici =csv.writer(dosya)
+    yazici.writerow(["Ad","Not"])
+    yazici.writerows(ogrenciler)
+
+
+with open("notlar.csv","r", newline="") as dosya:
+    okuyucu = csv.reader(dosya)
+    baslik = next(okuyucu)  # Başlık satırını atla
+    for satir in okuyucu:
+        print(satir)
