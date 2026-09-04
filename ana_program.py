eacbfe5011b5490f0fa0e84bd51736282a39acb9
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-09-04 20:07:33 +0300

    Json başlangıcı

--- ana_program.py
+++ ana_program.py
@@ -1,30 +1,7 @@
-import csv
+import json
 
-# Öğrenci verilerini içeren liste
-ogrenciler = [["isim","ders","not"],["Ali","Matematik",90],["Ayşe","Fizik",85],["Mehmet","Kimya",78],["Fatma","Biyoloji",92],["Ahmet","Tarih",88]]
-
-with open("ogrenciler.csv", "w", newline="",encoding="utf-8") as dosya:
-    yazici = csv.writer(dosya)
-    yazici.writerows(ogrenciler)
-
-print("ogrenciler.csv dosyası oluşturuldu ve veriler yazıldı.")
-
-with open("ogrenciler.csv", "r", encoding="utf-8") as dosya:
-    okuyucu = csv.DictReader(dosya)
-    satirlar = list(okuyucu)
-
-for satir in satirlar:
-    print(satir)
-
-print("ogrenciler.csv dosyasındaki veriler okundu ve ekrana yazdırıldı.")
-
-print("\n85 ve üzeri not alan öğrenciler:")
-basarili_ogrenciler = [s for s in satirlar if int(s['not']) >= 85]
-for s in basarili_ogrenciler:
-    print(f"- {s['isim']} ({s['not']})")
-
-print("\n Oratalama notu hesaplanıyor...")
-
-notlar =[int(s['not']) for s in satirlar]
-ortalama = sum(notlar) / len(notlar)
-print(f"Ortalama not: {ortalama:.2f}") 
+veri = {"kitaplar": ["simyacı", "suç ve ceza", "savaş ve barış", "1984", "hayvan çiftliği"], "toplam": 2}
+metin = json.dumps(veri)
+geri = json.loads(metin)
+print(geri)
+print(geri["kitaplar"], type(geri["toplam"]))
