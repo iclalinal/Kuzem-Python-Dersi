a9e62dd149ebf09d53b4616ee7ab356f78e422b1
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-09-04 20:22:52 +0300

    kitap_kaydet örneği

--- ana_program.py
+++ ana_program.py
@@ -1,7 +1,11 @@
 import json
 
-veri = {"kitaplar": ["simyacı", "suç ve ceza", "savaş ve barış", "1984", "hayvan çiftliği"], "toplam": 2}
-metin = json.dumps(veri)
-geri = json.loads(metin)
-print(geri)
-print(geri["kitaplar"], type(geri["toplam"]))
+def kitap_kaydet(kutuphane, dosya_adi):
+    with open(dosya_adi,"w", encoding="utf-8") as dosya:
+        json.dump(kutuphane, dosya, ensure_ascii=False, indent=4)
+
+
+kutuphane = {"kitaplar": ["simyacı", "suç ve ceza", "savaş ve barış", "1984", "hayvan çiftliği"], "toplam": 2}
+kitaplar = kutuphane["kitaplar"]
+
+kitap_kaydet(kitaplar, "kutuphane.json")
